e0874c32637a043d8b95275dc716f41f1c70bf7d
Jessica <jessicaesp2000@gmail.com>
2018-11-26 14:46:41 -0600

    Fixed bugs

---
+++ app/src/main/java/com/example/ramir/driverapp/util/DoubleArray.java
@@ -0,0 +1,38 @@
+package com.example.ramir.driverapp.util;
+
+public class DoubleArray<T, U> {
+
+    private T first;
+    private U second;
+
+    public DoubleArray() {
+    }
+
+    public DoubleArray(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int size() {
+        int size = 0;
+        if (first != null) size +=1;
+        if (second != null) size +=1;
+        return size;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+}
